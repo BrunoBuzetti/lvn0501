3b7a4c667d1d0971352eead8b879c913e1117a1c
Bruno <bruno.buzetti.dev@gmail.com>
2020-12-23 22:30:23 +0100

    challenge part01

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,9 +17,10 @@ const Header: React.FC<IHeaderProps> = ({ openModal }) => (
         <div>
           <button
             type="button"
-            onClick={() => {
+            onClick={
+              openModal
               /* TODO OPEN MODAL */
-            }}
+            }
           >
             <div className="text">Novo Prato</div>
             <div className="icon">
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -28,6 +28,8 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       // TODO LOAD FOODS
+      const response = await api.get('/foods');
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -38,6 +40,27 @@ const Dashboard: React.FC = () => {
   ): Promise<void> {
     try {
       // TODO ADD A NEW FOOD PLATE TO THE API
+      // const response = await api.get('/foods', {
+      //   ...food,
+      //   available: true,
+      // });
+      // const response = await api.get('/foods');
+
+      // console.log(response.data);
+
+      // setFoods([...foods, response.data]);
+
+      const id = foods.length + 1;
+
+      const newFood = {
+        ...food,
+        id,
+        available: true,
+      };
+
+      await api.post('/foods', newFood);
+
+      setFoods([...foods, newFood]);
     } catch (err) {
       console.log(err);
     }
@@ -47,10 +70,37 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     // TODO UPDATE A FOOD PLATE ON THE API
+    try {
+      // const response = await api.put(`/foods/${editingFood.id}`, {
+      //   ...editingFood,
+      //   ...food,
+      // });
+      // setFoods(
+      //   foods.map(mappedFood =>
+      //     mappedFood.id === editingFood.id
+      //       ? { ...response.data }
+      //       : { mappedFood },
+      //   ),
+      // );
+      const response = await api.put(`/foods/${editingFood.id}`, food);
+
+      const newfoods = foods.filter(index => index.id !== editingFood.id);
+
+      setFoods([...newfoods, response.data]);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
     // TODO DELETE A FOOD PLATE FROM THE API
+    try {
+      await api.delete(`/foods/${id}`);
+
+      setFoods(foods.filter(food => food.id !== id));
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   function toggleModal(): void {
@@ -63,6 +113,8 @@ const Dashboard: React.FC = () => {
 
   function handleEditFood(food: IFoodPlate): void {
     // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
